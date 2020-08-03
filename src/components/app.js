ef836b41efe6461c7482e4abf4d0d88a6bdea89e
Nash Hale <you@example.com>
2020-08-03 17:51:38 -0600

    commit on master

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import moment from "moment";
-import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 
 import PortfolioContainer from "./portfolio/portfolio-container";
@@ -10,21 +8,21 @@ import About from "./pages/about";
 import Contact from "./pages/contact"; 
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail"
+import Auth from"./pages/auth";
 import NoMatch from "./pages/no-match"
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='container'>
 
         <Router>
           <div>
-          <h1>Nash Hale Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
             <NavigationContainer />
 
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
