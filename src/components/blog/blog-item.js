e67645cf8d2652c2429591b8c9327ff20658b317
Nash Hale <you@example.com>
2020-08-16 12:59:34 -0600

    commit

---
+++ src/components/blog/blog-item.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Blog from "../pages/blog";
+
+const BlogItem = (props) => {
+  const {
+    id,
+    title,
+    content,
+    blog_status,
+    featured_image_url,
+  } = props.blogItem;
+
+  return (
+    <div>
+      <Link to={`/b/${id}`}>
+        <h1>{title}</h1>
+      </Link>
+      <div>{content}</div>
+    </div>
+  );
+};
+
+export default BlogItem;
